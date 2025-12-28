a2468cfb9e017fb2a88e05d43e90f1fd9312ec7f
Missingway <ttw.missingway@gmail.com>
2025-12-28 12:21:36 -0600

    init

---
+++ src/app.d.ts
@@ -0,0 +1,21 @@
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+declare global {
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		interface Platform {
+			env?: {
+				// Add your Cloudflare environment variables here
+				// Example: VITE_PRISMIC_ENVIRONMENT?: string;
+			};
+			context?: {
+				waitUntil(promise: Promise<unknown>): void;
+			};
+			caches?: CacheStorage & { default: Cache };
+		}
+	}
+}
+
+export {};
